b6755732ed8f94276b83b0d54a4618d82d5c912f
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-02-21 01:02:22 -0600

    More scientific operation implementations got added, for this purpose, 2 Regex patterns got added in order to implement a more "live validation" for typing invalid expressions like triple negative signs or double operators (--- or ÷÷), also add a regex to evaluate base n logarithms and add switch case for custom displaying of operations in screen and also for custom math validations in the operations.

--- app/src/main/java/com/example/holamundo/MainActivity.java
+++ app/src/main/java/com/example/holamundo/MainActivity.java
@@ -16,12 +16,27 @@ public class MainActivity extends AppCompatActivity {
     TextView resultScreen;
     Pattern twoNumbersOperationsPattern;
     Pattern singleNumberOperationsPattern;
+    Pattern singleOperatorPattern;
+    Pattern negativeSignPattern;
+    Pattern logPattern;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        twoNumbersOperationsPattern = Pattern.compile("(\\-?\\d*\\.?\\d+)([\\+\\-÷%x\\^])(\\-?\\d*\\.?\\d+)"); // operation regex
-        singleNumberOperationsPattern = Pattern.compile("(\\btan|\\bcos|\\bsin|\\b1\\/x|\\blog|\\bln)(\\-?\\d*\\.?\\d+)"); // second operation regex
-        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
+        String regex2Numbers = "(\\-?\\d*\\.?\\d+)([\\+\\-÷%x\\^])(\\-?\\d*\\.?\\d+)";
+        String regex1Number = "(\\btan|\\bcos|\\bsin|\\btanh|\\bcosh|\\bsinh|\\b1\\/x|\\blog10\\(|\\bln|\\bsqrt\\()(\\-?\\d*\\.?\\d+)";
+        String regex0DoubleOperators = "[%x÷\\+\\.\\/^]$|(\\blog)$";
+        String regexMinus = "^[\\-]$|[\\-]{2}$";
+        String logRegex = "log\\((\\-?\\d*\\.?\\d+)\\)(\\-?\\d*\\.?\\d+)";
+
+        System.out.println(regex2Numbers + "\n" + regex1Number + "\n" + regex0DoubleOperators + "\n" + regexMinus + "\n" + logRegex);
+        twoNumbersOperationsPattern = Pattern.compile("(\\-?\\d*\\.?\\d+)([\\+\\-÷%x\\^]|log)(\\-?\\d*\\.?\\d+)"); // operation regex
+        singleNumberOperationsPattern = Pattern.compile("(\\btan|\\bcos|\\bsin|\\btanh|\\bcosh|\\bsinh|\\b1\\/x|\\blog10\\(|\\bln|\\bsqrt\\()(\\-?\\d*\\.?\\d+)"); // second operation regex
+        singleOperatorPattern = Pattern.compile("[%x÷\\+\\.\\/^]$|(\\blog)$");// match all operation signs except minus (signs) at the end of the string
+        negativeSignPattern = Pattern.compile("^[\\-]$|[\\-]{2}$");// match a negative sign if its at both start and end of string, or double sign at the end of string
+        logPattern = Pattern.compile("log\\((\\-?\\d*\\.?\\d+)\\)(\\-?\\d*\\.?\\d+)");// regex for operating with logarithm base n
+
+
+        supportRequestWindowFeature(Window.FEATURE_NO_TITLE);// removes title bar
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         resultScreen = findViewById(R.id.tvResultado);
@@ -55,7 +70,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private String processSymbolWriting(String pressedSymbol, String screenText) {
-        if (screenText.isEmpty()) return pressedSymbol;
+        switch (pressedSymbol) {
+            // no double characters allowed without exception
+            case "%":
+            case "x":
+            case "÷":
+            case "+":
+            case "/":
+            case "log":
+            case "^":
+                if (singleOperatorPattern.matcher(screenText).find() || screenText.isEmpty()) {
+                    Toast.makeText(this, "operador no permitido aquí", Toast.LENGTH_SHORT).show();
+                    return screenText;
+                }
+                break;
+            case ".":
+                if (singleOperatorPattern.matcher(screenText).find()) {
+                    Toast.makeText(this, "operador no permitido aquí", Toast.LENGTH_SHORT).show();
+                    return screenText;
+                } else
+                    return screenText + pressedSymbol;
+
+                // double character allowed in middle of expression, but not at the start
+            case "-":
+                if (negativeSignPattern.matcher(screenText).find()) {
+                    Toast.makeText(this, "no mas signos negativos", Toast.LENGTH_SHORT).show();
+                    return screenText;
+                }
+        }
+
         return screenText + pressedSymbol;
     }
 
@@ -66,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
     public void operate(View view) {
         Matcher twoNumbersOperationMatcher = twoNumbersOperationsPattern.matcher(getScreenString());
         Matcher singleNumberOperationMatcher = singleNumberOperationsPattern.matcher(getScreenString());
+        Matcher logarithmMatcher = logPattern.matcher(getScreenString());
 
         if (twoNumbersOperationMatcher.matches())
             try {
@@ -78,7 +122,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
 
-         else if (singleNumberOperationMatcher.matches())
+        else if (singleNumberOperationMatcher.matches())
             try {
                 String operator = singleNumberOperationMatcher.group(1);
                 float number1 = toFloat(singleNumberOperationMatcher.group(2));
@@ -87,7 +131,17 @@ public class MainActivity extends AppCompatActivity {
             } catch (Exception e) {
                 Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
             }
-         else
+        else if (logarithmMatcher.matches()) {
+            try {
+                float number1 = toFloat(logarithmMatcher.group(1));
+                float number2 = toFloat(logarithmMatcher.group(2));
+                String result = String.valueOf(getResult(number1, number2, "log"));
+                resultScreen.setText(result);
+            }catch(Exception e){
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        } else
+
             Toast.makeText(this, "Input no valido", Toast.LENGTH_LONG).show();
 
     }
@@ -99,7 +153,7 @@ public class MainActivity extends AppCompatActivity {
                 result = number1 * number2;
                 break;
             case "÷":
-                if (number2 == 0.0f) throw new Exception("No se puede dividir entre 0");
+                if (number2 == 0.0f) throw new Exception("No se puede dividir entre cero");
                 result = number1 / number2;
                 break;
             case "%":
@@ -112,8 +166,14 @@ public class MainActivity extends AppCompatActivity {
                 result = number1 + number2;
                 break;
             case "^":
-                if (number1 == 0.0f) throw new Exception("0^0 no esta definido");
+                if (number1 == 0.0f) throw new Exception("0^0 no definido");
                 result = (float) Math.pow(number1, number2);
+                break;
+
+            case "log": // logb(n) = loge(n) / loge(b) math identity
+                if (number1 <= 1.0f || number2 <= 0.0f)
+                    throw new Exception("No se puede resolver log(".concat(String.valueOf(number1)).concat(")").concat(String.valueOf(number2)));
+                result = (float) Math.log(number2) / (float) Math.log(number1);
                 break;
             default:
                 result = 0.0f;
@@ -125,25 +185,38 @@ public class MainActivity extends AppCompatActivity {
         double result = 0.0f;
         switch (operation) {
             case "cos":
-                result = Math.cos(number);
+                result = Math.cos(Math.toRadians(number));
                 break;
             case "tan":
-                result = Math.tan(number);
+                result = Math.tan(Math.toRadians(number));
                 break;
             case "sin":
-                result = Math.sin(number);
+                result = Math.sin(Math.toRadians(number));
+                break;
+            case "cosh":
+                result = Math.cosh(number);
+                break;
+            case "tanh":
+                result = Math.tanh(number);
+                break;
+            case "sinh":
+                result = Math.sinh(number);
                 break;
             case "ln":
                 if (number == 0.0f) throw new Exception("ln(0) no esta definido");
                 result = Math.log(number);
                 break;
-            case "log":
+            case "log10(":
                 if (number == 0.0f) throw new Exception("log10(0) no esta definido");
                 result = Math.log10(number);
                 break;
             case "1/x":
                 if (number == 0.0f) throw new Exception("El cero no tiene inverso multiplicativo");
                 result = Math.pow(number, -1);
+                break;
+            case "sqrt(":
+                if (number < 0) throw new Exception("No existe raiz cuadrada de negativos");
+                result = Math.sqrt(number);
                 break;
         }
         return (float) result;
@@ -172,14 +245,24 @@ public class MainActivity extends AppCompatActivity {
             case "sin":
             case "cos":
             case "tan":
+            case "sinh":
+            case "cosh":
+            case "tanh":
             case "1/x":
+            case "ln":
                 newScreenText = buttonText;
+                break;
+            case "√":
+                newScreenText = "sqrt(";
+                break;
+            case "log10":
+                newScreenText = "log10(";
                 break;
             case "a^b":
                 newScreenText = screenText.concat("^");
                 break;
-            case "ln/log":
-                newScreenText = screenText.contains("log") ? newScreenText = screenText.replaceFirst("log", "ln") : screenText.contains("ln") ? newScreenText = screenText.replaceFirst("ln", "log") : "ln";
+            case "log":
+                newScreenText = "log(".concat(screenText).concat(")");
                 break;
             default:
                 newScreenText = "";
